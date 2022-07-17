4f73c154a8b22e59c6953d2e36595456f0874306
Shivangi Gupta <shivangiguptaoct11@gmail.com>
2022-07-17 14:40:16 +0530

    &amp;

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -5,32 +5,32 @@ export default function Header() {
   return (
     <div><nav className="navbar navbar-expand-lg bg-secondary ">
     <div className="container-fluid ">
-      <Link className="nav-link text-white" to='/'>Shivangi Gupta</Link>      
+      <Link className="nav-link text-white" to='/' title="Shivangi Gupta">Shivangi Gupta</Link>      
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
           <li className="nav-item">
-            <Link className="nav-link active text-white" aria-current="page" to='/workexperience'>Work Experience</Link>
+            <Link className="nav-link active text-white" aria-current="page" to='/workexperience' title="Work Experience">Work Experience</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white" to='/technicalskills'>Technical Skills</Link>
+            <Link className="nav-link text-white" to='/technicalskills' title="Technical Skills">Technical Skills</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white"  to='/education'>
+            <Link className="nav-link text-white"  to='/education' title="Education">
               Education
             </Link>
             
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white" to='/awardshonors'>Awards and honors</Link>
+            <Link className="nav-link text-white" to='/awardshonors' title="Awards &amp; honor">Awards &amp; honors</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white" to='/careerhighlight'>Career Highlight</Link>
+            <Link className="nav-link text-white" to='/careerhighlight'  title="Career Hightlights">Career Highlights</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link text-white" to='/projects'>Projects</Link>
+            <Link className="nav-link text-white" to='/projects'  title="Projects">Projects</Link>
           </li>
         </ul>
-        <Link className="nav-link text-white" to='/contactdetails'>Contact Details</Link>
+        <Link className="nav-link text-white" to='/contactdetails'  title="Contact Details">Contact Details</Link>
       </div>
     </div>
   </nav></div>
