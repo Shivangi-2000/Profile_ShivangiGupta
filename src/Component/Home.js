ffb4c25ca32fd243a0c4e2f12d6236de29aaf4de
Shivangi Gupta <shivangiguptaoct11@gmail.com>
2022-07-19 15:31:45 +0530

    Work Experience

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,44 +1,48 @@
 import React from 'react'
+import WorkExperience from './WorkExperience'
 
 export default function Home() {
-  return (
-    <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+  return (<>
+    <div id="carouselExampleCaptions" className="carousel slide" data-bs-ride="false">
+  <div className="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
-  <div class="carousel-inner" style={{height:'600px'}} >
-    <div class="carousel-item active" >
-      <img src="Profile2.jpg" class="d-block w-100" alt="..." />
-      <div class="carousel-caption d-none d-md-block" style={{padding:'120px'}}>
-        <h5>Shivangi Gupta</h5>
+  <div className="carousel-inner" style={{height:'600px'}} >
+    <div className="carousel-item active" >
+      <img src="Profile2.jpg" className="d-block w-100" alt="..." />
+      <div className="carousel-caption d-none d-md-block" style={{padding:'120px'}}>
+        <h5 >Shivangi Gupta</h5>
         <p>GIS Developer at TCS</p>
       </div>
     </div>
-    <div class="carousel-item">
-      <img src="Profile2.jpg" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block" style={{padding:'120px'}}>
+    <div className="carousel-item">
+      <img src="Profile2.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block" style={{padding:'120px'}}>
         <h5>Shivangi Gupta</h5>
         <p>A girl, highly motivated towards technology and innovation, working as a GIS Developer in TCS Limited, living in a city called "Banda" of Uttar Pradesh (India).</p>
       </div>
     </div>
-    <div class="carousel-item">
-      <img src="Profile2.jpg" class="d-block w-100" alt="..."/>
-      <div class="carousel-caption d-none d-md-block" style={{padding:'120px'}}>
+    <div className="carousel-item">
+      <img src="Profile2.jpg" className="d-block w-100" alt="..."/>
+      <div className="carousel-caption d-none d-md-block" style={{padding:'120px'}}>
         <h5>Shivangi Gupta</h5>
         <p>Beleive in yourself. beacuse you are the hero of your life</p>
       </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
+  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
+  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="visually-hidden">Next</span>
   </button>
+  
 </div>
+<WorkExperience/>
+</>
   )
 }
--- src/Component/WorkExperience.js
+++ src/Component/WorkExperience.js
@@ -1,7 +1,39 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import './style.css'
 export default function WorkExperience() {
   return (
-    <div>WorkExperience</div>
+    <div><nav className="navbar navbar-expand-lg bg-secondary ">
+    <div className="container-fluid ">
+      <Link className="nav-link text-white" to='/' title="Shivangi Gupta">Work Experience</Link>      
+    </div>
+  </nav>
+  <h1 id='heading'>Work Experience</h1>
+  <p id='subHeading'>Experience comes from practice</p>
+  <br></br>
+
+  <div className='row'>
+    <div className='col-md-6' id='animate'>
+        <div className="card" >
+          <img src="TCS.jpg" className="card-img-top" alt="..."/>
+          <div className="card-body">
+            <h2 id='exp'>GIS Developer, TCS India</h2>
+            <h4 id='expt'>Oct 2021 – Present</h4>
+            <p className="card-text" id='quoat'>Working on a Project "Hazard awarness &amp; Warning Center" as GIS Developer at Tata Consultancy Sevices Limited</p>
+          </div>
+        </div>
+      </div>
+      <div className='col-md-6'>
+        <div className="card" >
+          <img src="Novelvox.png" className="card-img-top" alt="..."/>
+          <div className="card-body">
+          <h3 id='exp'>Software Developer Intern, Novelvox Private Limited</h3>
+            <h4 id='expt'>June 2021 - Sept 2021</h4>
+            </div>
+      </div>
+    </div>
+  </div><br></br>
+  <p id='subHeading'>Life is not about knowing yourself. Life is about creating yourself........... </p>
+  </div>
   )
 }
